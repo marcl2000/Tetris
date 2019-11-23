8f9a18ddf4c67960af8d636cf0d6f0d8f495bbd4
Naomy Nancy Sabo <nnsabo@uwaterloo.ca>
2019-11-22 21:53:29 -0500

    User chnages when a block is done, re-read fstream when eof is reached

--- main.cc
+++ main.cc
@@ -4,14 +4,14 @@
 using namespace std;
 
 int main(int argc, char *argv[]){	
-	//Variables for program setup
+	// Variables for program setup
 	string numSteps = "";
 	int steps = 1;
 	string user1file = "biquadris_sequence1.txt";
 	string user2file = "biquadris_sequence2.txt";
   	int turn = 0;	
 
-	//If any command lines were given, interpret and apply them
+	// If any command lines were given, interpret and apply them
 	for (int i = 0; i < argc; ++i) {
 		string cmd = argv[i];
 		if (cmd.substr(0,2) == "-t") {
@@ -43,18 +43,10 @@ int main(int argc, char *argv[]){
         string u1block;
         string u2block;
 	
+	u1stream >> u1block;
+        cout << "user1's block is " << u1block << endl;
+	
 	while (true) {
-
-		//To differentiate between users, user1 will play when the turn count is
-        	//even and user2 will play when the turn count is odd
-        	if (turn % 2 == 0) {
-                	u1stream >> u1block;
-                	cout << "user1's block is " << u1block << endl;
-       		 } else {
-                	u2stream >> u2block;
-               		cout << "user2's block is " << u2block << endl;
-       		 }
-       		 ++turn;
 		
 		//Read the first string inputted
 		string s;
@@ -99,7 +91,28 @@ int main(int argc, char *argv[]){
                 } else if (s.substr(0, 2) == "do") {
                         cout << "Moving down by " << steps << " steps" << endl;
                 } else if (s.substr(0, 2) == "dr") {
-                        cout << "Dropping  " << steps << " steps" << endl;
+		  	cout << "Dropping  " << steps << " steps" << endl;
+			//To differentiate between users, user1 will play when the turn count is
+                	//even and user2 will play when the turn count is odd
+			++turn;
+			if (turn % 2 == 0) {
+                        	u1stream >> u1block;
+				if (u1stream.eof()) {
+					// Clear the filestream and start reading from the beginning of the file
+					u1stream.clear( );
+					u1stream.seekg( 0, std::ios::beg);
+					u1stream >> u1block;
+				}
+                        	cout << "user1's block is " << u1block << endl;
+               		 } else {
+                        	u2stream >> u2block;
+				if (u2stream.eof()) {
+					u2stream.clear( );
+                               		u2stream.seekg( 0, std::ios::beg);
+                                        u2stream >> u1block;
+                                }
+                       		cout << "user2's block is " << u2block << endl;
+                	 }
                 } else if (s.substr(0, 2) == "co") {
                         cout << "Rotating counter clockwise by " << steps << " steps" << endl;
                 } else if (s.substr(0, 2) == "cl") {
