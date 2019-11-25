00d5dd337b447747c8d88621c7d6d5d85f9ece5b
Claudia Marin <c3marin@uwaterloo.ca>
2019-11-25 18:33:59 -0500

    fixed :)

--- shape.cc
+++ shape.cc
@@ -5,33 +5,33 @@
 using namespace std;
 
 void Shape::move_left(int n){
-		for (int i=0; i < 4; ++i) {
-			int newx = this->members[i].getCoord().x - n * 20;
-			Coord c(newx, this->members[i].getCoord().y);
-			this->members[i].setCoord(c);
-			cout << "New coords are " << c.x << " , " << c.y << endl;
-		}
-		
-		for (int i=0; i < 4; ++i) {
-			while (this->members[i].getCoord().x < 0) {
-				this->move_right(1);
-			}	
-		}
+	for (int i=0; i < 4; ++i) {
+		int newx = this->members[i].getCoord().x - n * 20;
+		Coord c(newx, this->members[i].getCoord().y);
+		this->members[i].setCoord(c);
+		cout << "New coords are " << c.x << " , " << c.y << endl;
+	}
+
+	for (int i=0; i < 4; ++i) {
+		while (this->members[i].getCoord().x < 0) {
+			this->move_right(1);
+		}	
+	}
 }
 
 void Shape::move_right(int n){
-	 for (int i=0; i < 4; ++i) {
-         	int newx = this->members[i].getCoord().x + n * 20;
-                Coord c(newx, this->members[i].getCoord().y);
-                this->members[i].setCoord(c);
-                cout << "New coords are " << c.x << " , " << c.y << endl;
-         }
+	for (int i=0; i < 4; ++i) {
+		int newx = this->members[i].getCoord().x + n * 20;
+		Coord c(newx, this->members[i].getCoord().y);
+		this->members[i].setCoord(c);
+		cout << "New coords are " << c.x << " , " << c.y << endl;
+	}
 
-	 for (int i=0; i < 4; ++i) {
-                        while (this->members[i].getCoord().x > 10 * 20) {
-                                this->move_left(1);
-                        }	
-	 }
+	for (int i=0; i < 4; ++i) {
+		while (this->members[i].getCoord().x > 10 * 20) {
+			this->move_left(1);
+		}	
+	}
 }
 
 void Shape::move_down(){
@@ -47,7 +47,7 @@ void Shape::move_down(){
 //should change make_blind to false after dropping and change the cells in the shape to is_filled()
 void Shape::drop(){
 	//the main will delete the old shape
-	
+
 	//check to see if it can be dropped
 
 
@@ -125,90 +125,90 @@ void Shape::clockwise(){
 				}
 			}
 		}
-		if(this->type_name == "S"){
-				cout << "SHAPE IS S" << endl;
-			
-			// Check if it is horizontal, then it can be rotated always
-			if (this->members[0].getCoord().x - this->members[3].getCoord().x == 2) {
-				// Position 1
-				if (this->members[0].getCoord().y > this->members[3].getCoord().y) {
-			
+	}
+	if(this->type_name == "S"){
+		cout << "SHAPE IS S" << endl;
+
+		// Check if it is horizontal, then it can be rotated always
+		if (this->members[0].getCoord().x - this->members[3].getCoord().x == 2) {
+			// Position 1
+			if (this->members[0].getCoord().y > this->members[3].getCoord().y) {
+
 				cout << "POSITION 1" << endl;
-					Coord one(this->members[0].getCoord().x, this->members[0].getCoord().y - 2 * 20);
-					this->members[0].setCoord(one);
+				Coord one(this->members[0].getCoord().x, this->members[0].getCoord().y - 2 * 20);
+				this->members[0].setCoord(one);
 
-					Coord two(this->members[1].getCoord().x - 1 * 20, this->members[1].getCoord().y - 1 * 20);
-					this->members[1].setCoord(two);
+				Coord two(this->members[1].getCoord().x - 1 * 20, this->members[1].getCoord().y - 1 * 20);
+				this->members[1].setCoord(two);
 
-					//Third coordinate stays the same
+				//Third coordinate stays the same
 
-					Coord four(this->members[3].getCoord().x - 1 * 20, this->members[3].getCoord().y + 1 * 20);
-					this->members[3].setCoord(four);	
-				} else {
-
-
-					cout << "POSITION 2" << endl;
-					Coord one(this->members[3].getCoord().x, this->members[3].getCoord().y - 2 * 20);
-                                        this->members[3].setCoord(one);
-
-                                        Coord two(this->members[2].getCoord().x - 1 * 20, this->members[2].getCoord().y - 1 * 20);
-                                        this->members[2].setCoord(two);
-
-                                        //Third coordinate stays the same
-
-                                        Coord four(this->members[0].getCoord().x - 1 * 20, this->members[0].getCoord().x + 1 * 20);
-                                        this->members[0].setCoord(four);
-
-				}
-
+				Coord four(this->members[3].getCoord().x - 1 * 20, this->members[3].getCoord().y + 1 * 20);
+				this->members[3].setCoord(four);	
 			} else {
-				if (this->members[0].getCoord().y - this->members[3].getCoord().y == -2) {
-					
-					cout << "POSITION 3" << endl;
-					Coord one(this->members[0].getCoord().x + 2 * 20, this->members[0].getCoord().y + 1 * 20);
-					this->members[0].setCoord(one);
 
-					Coord two(this->members[1].getCoord().x + 1 *20, this->members[1].getCoord().y);
-					this->members[1].setCoord(two);
 
-					Coord three(this->members[2].getCoord().x, this->members[2].getCoord().y + 1 *20);
-					this->members[2].setCoord(three);
+				cout << "POSITION 2" << endl;
+				Coord one(this->members[3].getCoord().x, this->members[3].getCoord().y - 2 * 20);
+				this->members[3].setCoord(one);
 
-					Coord four(this->members[3].getCoord().x - 1 * 20, this->members[3].getCoord().y);
-					this->members[3].setCoord(four);
-				} else {
+				Coord two(this->members[2].getCoord().x - 1 * 20, this->members[2].getCoord().y - 1 * 20);
+				this->members[2].setCoord(two);
 
-					cout << "POSITION 2" << endl;
-					Coord one(this->members[0].getCoord().x - 1 * 20, this->members[0].getCoord().y);
-					this->members[0].setCoord(one);
+				//Third coordinate stays the same
 
-					Coord two(this->members[1].getCoord().x, this->members[1].getCoord().y + 1 * 20);
-					this->members[1].setCoord(two);
+				Coord four(this->members[0].getCoord().x - 1 * 20, this->members[0].getCoord().x + 1 * 20);
+				this->members[0].setCoord(four);
 
-					Coord three(this->members[2].getCoord().x + 1 * 20, this->members[2].getCoord().y);
-					this->members[2].setCoord(three);
-
-					Coord four(this->members[3].getCoord().x + 2 * 20, this->members[3].getCoord().y + 1 *20 );
-					this->members[3].setCoord(four);
-
-				}
-			
-			
 			}
-			
-		
+
+		} else {
+			if (this->members[0].getCoord().y - this->members[3].getCoord().y == -2) {
+
+				cout << "POSITION 3" << endl;
+				Coord one(this->members[0].getCoord().x + 2 * 20, this->members[0].getCoord().y + 1 * 20);
+				this->members[0].setCoord(one);
+
+				Coord two(this->members[1].getCoord().x + 1 *20, this->members[1].getCoord().y);
+				this->members[1].setCoord(two);
+
+				Coord three(this->members[2].getCoord().x, this->members[2].getCoord().y + 1 *20);
+				this->members[2].setCoord(three);
+
+				Coord four(this->members[3].getCoord().x - 1 * 20, this->members[3].getCoord().y);
+				this->members[3].setCoord(four);
+			} else {
+
+				cout << "POSITION 2" << endl;
+				Coord one(this->members[0].getCoord().x - 1 * 20, this->members[0].getCoord().y);
+				this->members[0].setCoord(one);
+
+				Coord two(this->members[1].getCoord().x, this->members[1].getCoord().y + 1 * 20);
+				this->members[1].setCoord(two);
+
+				Coord three(this->members[2].getCoord().x + 1 * 20, this->members[2].getCoord().y);
+				this->members[2].setCoord(three);
+
+				Coord four(this->members[3].getCoord().x + 2 * 20, this->members[3].getCoord().y + 1 *20 );
+				this->members[3].setCoord(four);
+
+			}
+
+
 		}
-		//if heavy is on, then this rotate should drop the shape by 1
-		if(this->heavy_flag){
-			for(int i=0;i<4;i++){
-				Coord c(this->members[i].getCoord().x, this->members[i].getCoord().y + 1);
-				this->members[i].setCoord(c);
-			}
+
+
+	}
+	//if heavy is on, then this rotate should drop the shape by 1
+	if(this->heavy_flag){
+		for(int i=0;i<4;i++){
+			Coord c(this->members[i].getCoord().x, this->members[i].getCoord().y + 1);
+			this->members[i].setCoord(c);
 		}
 	}
-
-	//if(this->type_name == "J"){
 }
+
+
 
 void Shape::counterclockwise(){
 
